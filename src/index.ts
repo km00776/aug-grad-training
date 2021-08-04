4d21f4fb6c921901e97a70f5bb1e5e7a4ad16eef
Murugathas <2033658@cognizant.com>
2021-08-04 10:55:19 +0100

    changed to html

--- src/index.ts
+++ src/index.ts
@@ -2,123 +2,123 @@ import * as _ from "lodash";
 import '../styles.css';
 
 window.addEventListener("load", () => {
-    const header = document.createElement("h1");
-    header.innerText = "Front End HTML and CSS Assignment"
-    const body = document.querySelector("body");
-    body.appendChild(header);
-    const fe = [{id: 0, name: "html"}, {id: 1, name: "About you"}]
-    const element = document.createElement("h2")
-    element.innerText = _.find(fe, {id: 1})?.name;
-    body.appendChild(element);
+        // const header = document.createElement("h1");
+        // header.innerText = "Front End HTML and CSS Assignment"
+        // const body = document.querySelector("body");
+        // body.appendChild(header);
+        // const fe = [{id: 0, name: "html"}, {id: 1, name: "About you"}]
+        // const element = document.createElement("h2")
+        // element.innerText = _.find(fe, {id: 1})?.name;
+        // body.appendChild(element);
 
-    const form = document.createElement("form");
-    form.setAttribute("method", "post");
+        // const form = document.createElement("form");
+        // form.setAttribute("method", "post");
+        
+        // const titleLabel = document.createElement("label");
+        // titleLabel.innerHTML = "Title";
+        // body.appendChild(titleLabel);
+        // //
+        // const titleArray = ["Please select", "Mr", "Mrs", "Ms", "Miss", "Dr", "Sir" , "Reverend", "Dame", "Lady", "Lord", "Professor"]
+        // const selectList = document.createElement("select");
     
-    const titleLabel = document.createElement("label");
-    titleLabel.innerHTML = "Title";
-    body.appendChild(titleLabel);
-    //
-    const titleArray = ["Please select", "Mr", "Mrs", "Ms", "Miss", "Dr", "Sir" , "Reverend", "Dame", "Lady", "Lord", "Professor"]
-    const selectList = document.createElement("select");
-   
 
-    for (let i = 0; i < titleArray.length; i++) {
-        var option = document.createElement("option");
-        option.value = titleArray[i];
-        option.text = titleArray[i];
-        selectList.appendChild(option);
-    }
-    form.appendChild(selectList);
-    //
-    const firstNameLabel = document.createElement("label");
-    firstNameLabel.innerHTML = "First Name";
-    form.appendChild(firstNameLabel);
+        // for (let i = 0; i < titleArray.length; i++) {
+        //     var option = document.createElement("option");
+        //     option.value = titleArray[i];
+        //     option.text = titleArray[i];
+        //     selectList.appendChild(option);
+        // }
+        // form.appendChild(selectList);
+        // //
+        // const firstNameLabel = document.createElement("label");
+        // firstNameLabel.innerHTML = "First Name";
+        // form.appendChild(firstNameLabel);
 
-    const firstNameInput = document.createElement("input");
-    firstNameInput.setAttribute('type', 'text');
-    firstNameInput.setAttribute("pattern", "[A-Za-z]");
-    form.append(firstNameInput);
+        // const firstNameInput = document.createElement("input");
+        // firstNameInput.setAttribute('type', 'text');
+        // firstNameInput.setAttribute("pattern", "[A-Za-z]");
+        // form.append(firstNameInput);
 
-    const lastNameLabel = document.createElement("label");
-    lastNameLabel.innerHTML = "Surname";
-    form.appendChild(lastNameLabel);
-  
-    const lastNameInput = document.createElement("input");
-    lastNameInput.setAttribute('type', 'text');
-    lastNameInput.setAttribute("pattern", "[A - Za - z]");
-    lastNameInput.setAttribute("oninvalid", "Please enter a valid last name")
-    form.append(lastNameInput);
+        // const lastNameLabel = document.createElement("label");
+        // lastNameLabel.innerHTML = "Surname";
+        // form.appendChild(lastNameLabel);
+    
+        // const lastNameInput = document.createElement("input");
+        // lastNameInput.setAttribute('type', 'text');
+        // lastNameInput.setAttribute("pattern", "[A - Za - z]");
+        // lastNameInput.setAttribute("oninvalid", "Please enter a valid last name")
+        // form.append(lastNameInput);
 
-    const dateOfBirthLabel = document.createElement("label");
-    dateOfBirthLabel.innerHTML = "Date of Birth";
-    form.appendChild(dateOfBirthLabel);
+        // const dateOfBirthLabel = document.createElement("label");
+        // dateOfBirthLabel.innerHTML = "Date of Birth";
+        // form.appendChild(dateOfBirthLabel);
 
-    const dateOfBirthInput = document.createElement("input");
-    dateOfBirthInput.setAttribute('type', 'date');
-    form.appendChild(dateOfBirthInput);
+        // const dateOfBirthInput = document.createElement("input");
+        // dateOfBirthInput.setAttribute('type', 'date');
+        // form.appendChild(dateOfBirthInput);
 
-    const phoneNumberLabel = document.createElement("label");
-    phoneNumberLabel.innerHTML = "Telephone number";
-    form.appendChild(phoneNumberLabel);
+        // const phoneNumberLabel = document.createElement("label");
+        // phoneNumberLabel.innerHTML = "Telephone number";
+        // form.appendChild(phoneNumberLabel);
 
-    const phoneNumberInput = document.createElement("input");
-    phoneNumberInput.setAttribute('type', 'tel');
-    phoneNumberInput.setAttribute("pattern", "/^\d{11}$/");
-    phoneNumberInput.setAttribute("oninvalid", "setCustomValidity('Please enter a valid UK Phone Number')")
-    form.appendChild(phoneNumberInput);
+        // const phoneNumberInput = document.createElement("input");
+        // phoneNumberInput.setAttribute('type', 'tel');
+        // phoneNumberInput.setAttribute("pattern", "/^\d{11}$/");
+        // phoneNumberInput.setAttribute("oninvalid", "setCustomValidity('Please enter a valid UK Phone Number')")
+        // form.appendChild(phoneNumberInput);
 
-    const emailLabel = document.createElement("label");
-    emailLabel.innerHTML = "Email Address";
-    form.appendChild(emailLabel);
+        // const emailLabel = document.createElement("label");
+        // emailLabel.innerHTML = "Email Address";
+        // form.appendChild(emailLabel);
 
-    const emailInput = document.createElement("input");
-    emailInput.setAttribute('type', 'email');
-    form.appendChild(emailInput);
+        // const emailInput = document.createElement("input");
+        // emailInput.setAttribute('type', 'email');
+        // form.appendChild(emailInput);
 
-    const submitBtn = document.createElement("input");
-    submitBtn.className = "btn";
-    submitBtn.id = "submitBtn"
+        // const submitBtn = document.createElement("input");
+        // submitBtn.className = "btn";
+        // submitBtn.id = "submitBtn"
 
-    submitBtn.setAttribute("type", "submit");
-    submitBtn.setAttribute("value", "Submit");
-    submitBtn.innerHTML = "Continue" ;
-    form.appendChild(submitBtn);
+        // submitBtn.setAttribute("type", "submit");
+        // submitBtn.setAttribute("value", "Submit");
+        // submitBtn.innerHTML = "Continue" ;
+        // form.appendChild(submitBtn);
 
-    const svgImg = document.createElement("img");
-    svgImg.src = "https://svgshare.com/i/Zn1.svg";
-    body.appendChild(svgImg);
+        // const svgImg = document.createElement("img");
+        // svgImg.src = "https://svgshare.com/i/Zn1.svg";
+        // body.appendChild(svgImg);
 
 
- 
+    
 
 
-    form.onsubmit = function () {
-        let firstName = firstNameInput.value;
-        let lastName = lastNameInput.value;
-        let dateOfBirth = dateOfBirthInput.value;
-        let phoneNumber = phoneNumberInput.value;
-        let title = selectList.value;
-        let emailAddress = emailInput.value;
+        // form.onsubmit = function () {
+        //     let firstName = firstNameInput.value;
+        //     let lastName = lastNameInput.value;
+        //     let dateOfBirth = dateOfBirthInput.value;
+        //     let phoneNumber = phoneNumberInput.value;
+        //     let title = selectList.value;
+        //     let emailAddress = emailInput.value;
 
-        if (firstName === "" || lastName === "" || dateOfBirth === "" || phoneNumber === "" || title === "" || emailAddress === "") {
-                alert("Empty fields - Please have all fields filled in")
-        }
+        //     if (firstName === "" || lastName === "" || dateOfBirth === "" || phoneNumber === "" || title === "" || emailAddress === "") {
+        //             alert("Empty fields - Please have all fields filled in")
+        //     }
 
-        if(title == titleArray[0]) {
-            alert("Select a valid Title");
-        }
-         
-        if ((/^\d{11}$/).test(phoneNumber) === false) {
-                alert("Please enter a valid UK number")
-        }
-        else {
-            alert(phoneNumber);
-        }
-       
-        return false;
-    }
+        //     if(title == titleArray[0]) {
+        //         alert("Select a valid Title");
+        //     }
+            
+        //     if ((/^\d{11}$/).test(phoneNumber) === false) {
+        //             alert("Please enter a valid UK number")
+        //     }
+        //     else {
+        //         alert(phoneNumber);
+        //     }
+        
+        //     return false;
+        // }
 
-    document.getElementsByTagName("body")[0].appendChild(form);
+        // document.getElementsByTagName("body")[0].appendChild(form);
 
    
 });
